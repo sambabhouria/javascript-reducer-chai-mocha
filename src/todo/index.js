a4923ed3ca4dea151ea8860eede2c780d215a912
Samba Diallo <samba.diallo@id-mac007.idtgv.cnit>
2021-05-21 12:14:49 +0200

    MacBook-Pro:javascript-reducer

---
+++ src/todo/index.js
@@ -0,0 +1,34 @@
+import { v4 as uuidv4 } from 'uuid';
+
+// reducer with array
+
+const todoReducer = (state, action) => {
+  switch (action.type) {
+    case 'DO_TODO':
+      return state.map(todo => {
+        if (todo.id === action.id) {
+          return { ...todo, complete: true };
+        } else {
+          return todo;
+        }
+      });
+    case 'UNDO_TODO':
+      return state.map(todo => {
+        if (todo.id === action.id) {
+          return { ...todo, complete: false };
+        } else {
+          return todo;
+        }
+      });
+    case 'ADD_TODO':
+      return state.concat({
+        task: action.task,
+        id: uuidv4(),
+        complete: false,
+      });
+    default:
+      return state;
+  }
+};
+
+export default todoReducer;
